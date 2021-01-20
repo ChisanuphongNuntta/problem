ab14ecd6dfc44d69d3f78c6c51d21d3405d01d08
ChisanuphongNuntta <tvrocker.www@gmail.com>
2021-01-20 15:42:51 +0700

    Heart

---
+++ problme3/Revers-stringjoin.py
@@ -0,0 +1,9 @@
+def reverse_str_join(s):
+    s1 = ''.join(reversed(s))
+    return s1
+
+
+input_str = 'INE-KMUTNB'
+
+if __name__=="__main__":
+    print('Reverse String using sliding =', reverse_str_join(input_str))
