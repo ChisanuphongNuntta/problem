2b74e6e9f477a1c5be5fe2d7102c3961c475e22b
Heart <tvrocker.www@gmail.com>
2021-03-16 14:41:47 +0700

    Heart

--- Problme7/BinaryTreeinsert.py
+++ Problme7/BinaryTreeinsert.py
@@ -18,6 +18,43 @@ class Node:
                     self.right.insert(data)
         else:
             self.data = data
+
+    def findval(self,lkpval):
+        if lkpval < self.data:
+            if self.left is None:
+                return str(lkpval)+" Not Found"
+            return self.left.findval(lkpval)
+        elif lkpval > self.data:
+            if self.right is None:
+                return str(lkpval)+" Not Found"
+            return self.right.findval(lkpval)
+        else:
+            print(str(self.data) + ' is found')
+    
+    def inorderTraversal(self,root):
+        res = []
+        if root:
+            res = self.inorderTraversal(root.left)
+            res.append(root.data)
+            res = res + self.inorderTraversal(root.right)
+        return res
+    
+    def PerorderTraversal(self,root):
+        res = []
+        if root:
+            res.append(root.data)
+            res = res + self.PerorderTraversal(root.left)
+            res = res + self.PerorderTraversal(root.right)
+        return res
+
+    def PostorderTraversal(self,root):
+        res = []
+        if root:
+            res = self.PostorderTraversal(root.left)
+            res = res + self.PostorderTraversal(root.right)
+            res.append(root.data)
+        return res
+
     def PrintTree(self):
         if self.left:
             self.left.PrintTree()
@@ -25,10 +62,16 @@ class Node:
         if self.right:
             self.right.PrintTree()
 
+
 root = Node(12)
-root.insert(6)
+root.insert(27)
 root.insert(14)
-root.insert(3)
+root.insert(35)
+root.insert(10)
+root.insert(19)
+root.insert(31)
+root.insert(42)
+print(root.inorderTraversal(root))
 
 root.PrintTree()
 
